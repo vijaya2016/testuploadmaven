de5982381f3e3fdc982eb77f782f5cdfa7ec3c84
vijaya <r.vijaya.2016@gmail.com>
2017-04-13 08:40:18 -0700

    testing upload maven projectcs

---
+++ src/test/java/com/json/practice/com/selenium/jenkins/integraion/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.json.practice.com.selenium.jenkins.integraion;
+
+public class HelloWorld {
+public static void main(String args[])
+{
+	System.out.println("hello QE Automation world ");
+}
+}
